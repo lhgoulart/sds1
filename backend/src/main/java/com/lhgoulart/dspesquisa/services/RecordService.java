40a1db9584df6bbeccb50f982920578544479aa3
unknown <lhgoulart92@gmail.com>
2020-09-14 12:01:06 -0300

    Post records

---
+++ backend/src/main/java/com/lhgoulart/dspesquisa/services/RecordService.java
@@ -0,0 +1,38 @@
+package com.lhgoulart.dspesquisa.services;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lhgoulart.dspesquisa.dto.RecordDTO;
+import com.lhgoulart.dspesquisa.dto.RecordInsertDTO;
+import com.lhgoulart.dspesquisa.entities.Game;
+import com.lhgoulart.dspesquisa.entities.Record;
+import com.lhgoulart.dspesquisa.repositories.GameRepository;
+import com.lhgoulart.dspesquisa.repositories.RecordRepository;
+
+@Service
+public class RecordService {
+
+	@Autowired
+	private RecordRepository repository;
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	@Transactional
+	public RecordDTO insert(RecordInsertDTO dto) {
+		Record entity = new Record();
+		entity.setName(dto.getName());
+		entity.setAge(dto.getAge());
+		entity.setMoment(Instant.now());
+		
+		Game game = gameRepository.getOne(dto.getGameId());
+		entity.setGame(game);
+		
+		entity = repository.save(entity);		
+		return new RecordDTO(entity);
+	}
+}
